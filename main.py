41c2f40c1faabe2cdb2732de9f3eec6d197b25cf
Devon Sawatsky <novedevo@gmail.com>
2021-07-29 14:30:11 -0700

    tutorial

---
+++ main.py
@@ -0,0 +1,23 @@
+import urwid
+
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+
+def show_or_exit(key):
+    exit_on_q(key)
+    txt.set_text(repr(key))
+
+palette = [
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark red'),
+    ('bg', 'black', 'dark blue'),
+    ]
+
+txt = urwid.Text(("banner", u" Hello World "), align="center")
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+
+loop.run()

a113d537aee07ef515ab1f2983cdede42dab1671
Devon Sawatsky <novedevo@gmail.com>
2021-07-29 15:56:28 -0700

    MVP cli singleshot addition

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def exit_on_q(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
 
-calcpalette = [
-    ('bg', 'white', 'black'),
-    ('numpad', 'black', 'light blue'),
-    ('display', 'green', 'dark gray')
-]
+class QuestionBox(urwid.Filler):
+    def keypress(self, size, key):
+        if key != 'enter':
+            if key in tuple(map(lambda x: str(x), range(0,9))) or key == '+':
+                return super(QuestionBox, self).keypress(size, key)
+            return
+        self.original_widget = urwid.Text(f"= \n{eval(edit.edit_text)}.\n\nPress Q to exit.")
+
+edit = urwid.Edit("Enter your expression to be evaluated\n")
+fill = QuestionBox(edit)
+loop = urwid.MainLoop(fill, unhandled_input=exit_on_q)
+loop.run()
